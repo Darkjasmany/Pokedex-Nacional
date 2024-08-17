432659df212810a6ca4abc52b04d74961a8f2e4d
Jasmany Franco <jasmanyfranco@gmail.com>
2024-08-17 08:05:55 -0500

    Implementando Functionalidad para que muestre los pokemons consultando a la API y funcionalidad de botones de tipos para filtrar

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,10 @@
 const listaPokemon = document.querySelector("#listaPokemon");
+const btnHeaderTipos = document.querySelectorAll("#navegacion-botones button");
+const btnTodos = document.querySelector("#ver-todos");
 let URL = "https://pokeapi.co/api/v2/pokemon/";
+const CANTIDADPOKEMON = 151;
 
-for (let i = 1; i <= 6; i++) {
+for (let i = 1; i <= CANTIDADPOKEMON; i++) {
     // concatena el indice al final de la url
     fetch(URL + i)
         .then((respuesta) => respuesta.json())
@@ -10,12 +13,27 @@ for (let i = 1; i <= 6; i++) {
 }
 
 const mostrarPokemon = (data) => {
+    // Obtengo los tipos recorriendo el JSON y para obtener cada uno de los tipos creo un nuevo arreglo con los tipos y con el resultado de una vez creo los P
+    let tipos = data.types.map(
+        (type) => `<p class="bg-${type.type.name}">${type.type.name}</p> `
+    );
+    tipos = tipos.join(""); // Uno los dos elementos
+    // console.log(tipos);
+
+    // PARA AÑADIR NUMERO ANTES
+    let pokeId = data.id.toString();
+    if (pokeId.length === 1) {
+        pokeId = "00" + pokeId;
+    } else if (pokeId.length === 2) {
+        pokeId = "0" + pokeId;
+    }
+
     const divPokemon = document.createElement("DIV");
     divPokemon.classList.add(
         "pokemon",
         "rounded-2xl",
         "bg-white",
-        "shadow-2xl",
+        "shadow",
         "py-4",
         "uppercase",
         "relative",
@@ -26,7 +44,7 @@ const mostrarPokemon = (data) => {
  <p
                     class="pokemon-id-back absolute top-4 left-1/2 translate-x-[-50%] font-bold text-8xl text-gray-100 z-[-1]"
                 >
-                    #${data.id}
+                    #${pokeId}
                 </p>
                 <div class="pokemon-imagen ps-4 flex justify-center">
                     <img
@@ -44,7 +62,7 @@ const mostrarPokemon = (data) => {
                         <p
                             class="pokemon-id bg-gray-100 py-1 px-2 rounded-[100vmax] text-xs font-bold"
                         >
-                            #${data.id}
+                            #${pokeId}
                         </p>
                         <h2 class="pokemon-nombre text-2xl font-bold">
                             ${data.name}
@@ -54,8 +72,7 @@ const mostrarPokemon = (data) => {
                         class="pokemon-tipos flex gap-2 text-sm font-semibold flex-wrap justify-center
                         [&>p]:py-1 [&>p]:px-2 [&>p]:rounded-[100vmax]"
                     >
-                        <p class="tipo bg-electric">BUG</p>
-                        <p class="tipo bg-steel">STEEL</p>
+                        ${tipos}
                     </div>
                     <div
                         class="pokemon-stat flex gap-4 text-sm [&>p]:bg-gray-100 [&>p]:py-1 [&>p]:px-2 [&>p]:rounded-[100vmax]"
@@ -68,3 +85,34 @@ const mostrarPokemon = (data) => {
 
     listaPokemon.appendChild(divPokemon);
 };
+
+// console.log(btnHeaderTipos);
+// console.log(btnTodos);
+
+// if (btnTodos) {
+//     fetch(URL + i)
+//         .then((respuesta) => respuesta.json())
+//         // .then((datos) => console.log(datos));
+//         .then((data) => mostrarPokemon(data));
+// }
+
+// OBTENER EL ID PARA FILTRAL LA NEVAGCION
+btnHeaderTipos.forEach((btn) =>
+    btn.addEventListener("click", (e) => {
+        const botonId = e.currentTarget.id; // Obtengo el id de cada uno de los botones que presiono
+        listaPokemon.innerHTML = "";
+
+        for (let i = 1; i <= CANTIDADPOKEMON; i++) {
+            // concatena el indice al final de la url
+            fetch(URL + i)
+                .then((respuesta) => respuesta.json())
+                // .then((datos) => console.log(datos));
+                .then((data) => {
+                    const tipos = data.types.map((type) => type.type.name);
+                    if (tipos.some((tipo) => tipo.includes(botonId))) {
+                        mostrarPokemon(data);
+                    }
+                });
+        }
+    })
+);
